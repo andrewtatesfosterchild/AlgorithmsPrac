12e65ac0464801ecd1926de1594523cefe9008fa
Gaurav <harishtheballer@gmail.com>
2023-05-31 06:19:27 +0530

    easy prob hashmap approach

---
+++ Linear Search/pangramornot.py
@@ -0,0 +1,18 @@
+class Solution:
+    def checkIfPangram(self, sentence: str) -> bool:
+
+        # Logic: Simple Hashmao approach
+
+        '''
+            Won't even explain bro see the code
+        '''
+
+        hashmap = {'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0, 'f': 0, 'g': 0, 'h': 0, 'i': 0, 'j': 0, 'k':0, 'l': 0, 'm': 0, 'n': 0, 'o': 0, 'p': 0, 'q': 0, 'r': 0, 's': 0, 't': 0, 'u': 0, 'v': 0, 'w': 0, 'x': 0, 'y': 0, 'z': 0}
+
+
+        for i in range(len(sentence)):
+            hashmap[sentence[i]] = 1
+
+            
+
+        return False if 0 in hashmap.values() else True
