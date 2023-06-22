a95c2751bc8ead4cc340776ec6b38fc3ee57cca7
Gaurav <harishtheballer@gmail.com>
2023-06-22 13:08:46 +0530

    tail recursion & helper functions

---
+++ Recursion/numberofzeros.py
@@ -0,0 +1,19 @@
+def zerochecker(n , count):
+    if n == 0:
+        return count
+    
+    rem = n % 10
+
+    if rem == 0:
+        count+=1
+
+    return zerochecker (n//10 , count)
+
+def numberofzeros(n):
+    count = 0
+    return zerochecker (n , count)
+
+print (numberofzeros(1001))
+
+
+    
