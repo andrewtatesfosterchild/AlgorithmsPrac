2bd5cc4697ae3c19c4342fa2e62c21f325f2a171
Gaurav <harishtheballer@gmail.com>
2023-07-05 21:32:20 +0530

    god'srecursion

--- Recursion/Strings, Subsequences Recursion/removeAll'a's.py
+++ Recursion/Strings, Subsequences Recursion/removeAll'a's.py
@@ -1,18 +1,39 @@
 # Tail recursion ftw
 
-def helper(string , pos , res):
+# def helper(string , pos , res):
+#     if pos == len(string):
+#         return res
+    
+#     if string[pos] != 'a':
+#         res = res + string[pos]
+
+#     return helper(string , pos+1 , res)
+
+# def charremover(string):
+#     res = ""
+#     pos = 0
+#     return helper(string , pos , res)
+    
+
+# print(charremover("recursionezaf"))
+
+
+### Daddy recursion ###
+
+def charremover(string , pos):
+    res = ""
+
     if pos == len(string):
         return res
-    
+
     if string[pos] != 'a':
         res = res + string[pos]
-
-    return helper(string , pos+1 , res)
-
-def charremover(string):
-    res = ""
-    pos = 0
-    return helper(string , pos , res)
     
+    answerfrombelowcalls = charremover(string , pos+1)
 
-print(charremover("recursionezaf"))
+    res = res + answerfrombelowcalls
+    
+    return res
+
+
+print(charremover("daddyrecursion" , 0))
