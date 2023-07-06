4e7ea64ef7e6680fd7f58cb40f7916a008abbacc
Gaurav <harishtheballer@gmail.com>
2023-07-06 11:00:34 +0530

    modified

--- Recursion/Strings, Subsequences Recursion/conditionalAppleRemoval.py
+++ Recursion/Strings, Subsequences Recursion/conditionalAppleRemoval.py
@@ -4,7 +4,7 @@ def eraserofapifnotapple(string):
         return ""
 
     if string.startswith("ap") and not string.startswith("apple"):
-        return eraserofapifnotapple(string[5:])
+        return eraserofapifnotapple(string[2:])
     else:
         return string[0] + eraserofapifnotapple(string[1:])
 
