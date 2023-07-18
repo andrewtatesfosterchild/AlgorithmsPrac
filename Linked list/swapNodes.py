71a309bb7dc67e4c519108f4518b97f8ed10a3dc
Gaurav <harishtheballer@gmail.com>
2023-07-18 08:16:09 +0530

    much better approach

--- Linked list/swapNodes.py
+++ Linked list/swapNodes.py
@@ -47,37 +47,53 @@ class Solution:
     '''
     def swapNodes(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
 
-        if not head or not head.next:
-            return head
+        # if not head or not head.next:
+        #     return head
         
-        ptr1 = head
-        tail1 , tail2 = None , None
-        for _ in range(k - 1):
-            tail1 = ptr1
-            ptr1 = ptr1.next
+        # ptr1 = head
+        # tail1 , tail2 = None , None
+        # for _ in range(k - 1):
+        #     tail1 = ptr1
+        #     ptr1 = ptr1.next
         
-        swap1 = ptr1
+        # swap1 = ptr1
         
-        swap2 = head
+        # swap2 = head
 
-        while ptr1.next:
-            tail2 = swap2
-            swap2 = swap2.next
-            ptr1 = ptr1.next
+        # while ptr1.next:
+        #     tail2 = swap2
+        #     swap2 = swap2.next
+        #     ptr1 = ptr1.next
 
-        if swap1 == swap2:
-            return head
+        # if swap1 == swap2:
+        #     return head
         
-        if not tail1:
-            head = swap2
-        else:
-            tail1.next = swap2
+        # if not tail1:
+        #     head = swap2
+        # else:
+        #     tail1.next = swap2
         
-        if not tail2:
-            head = swap1
-        else:
-            tail2.next = swap1
+        # if not tail2:
+        #     head = swap1
+        # else:
+        #     tail2.next = swap1
         
-        swap1.next , swap2.next = swap2.next , swap1.next
+        # swap1.next , swap2.next = swap2.next , swap1.next
+
+        # return head
+
+                # logic: Much better one pass solution
+
+        n1 , n2 , p = None, None, head
+
+        while p:
+            k-=1
+            n2 = None if n2 == None else n2.next # **Very imp line to avoid Nonetype has no next error, Fundamental for future use
+            if k == 0: # This actually finds the kth node from the end in one loop rather the other approach which we followed earlier which takes two loops, Same logic tho, Once p is a kth node, You stat moving n2 from head so when p reaches the end node , n1 holds the kth node from the head and n2 holds the kth node from the beginning, Man I love this logic! ** Fundamental v imp do this. BuT wOnT iT seT n1 EvEryTime aFtEr k rEaCheD 0? in next iteration k is -1 and so on, so no it wont.
+                n1 = p
+                n2 = head
+            p = p.next
+
+        n1.val , n2.val = n2.val , n1.val # We are asked to swap values, not nodes
 
         return head
