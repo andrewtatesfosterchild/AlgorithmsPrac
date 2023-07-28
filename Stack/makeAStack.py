06fb9ecbacd576015dd8cd2db582a65327bd9749
Gauravpadam <1032201077@tcetmumbai.in>
2023-07-28 14:20:36 +0530

    Tad bit of changes

--- Stack/makeAStack.py
+++ Stack/makeAStack.py
@@ -12,22 +12,21 @@ class Stack:
         print(f"Stack of {n} elements created")
     
     def push(self , item):
-        if self.isFull(item):
+        if self.isFull():
             self.ptr+=1
             self.nums[self.ptr] = item
             return (f"{item} inserted")
-        else:
-            return "Stack Overflow"
+        
     def pop(self):
-        if not self.isEmpty():
+        if self.isEmpty():
             remove = self.nums[self.ptr]
-            self.nums = self.nums[:-1]
+            self.nums[self.ptr] = None
             self.ptr-=1
             return (f"{remove}  removed")
         else:
             return "Stack is empty"
         
-    def isFull(self , item):
+    def isFull(self):
         if self.ptr < len(self.nums) - 1:
             return True
         else:
@@ -40,51 +39,40 @@ class Stack:
             return False
         
     def peek(self):
-        try:
+        if self.isEmpty():
             return self.nums[self.ptr]
-        except:
-            return("Stack is empty")
+        else:
+            return "Stack is empty"
 
-class DynamicStack(Stack):
-    def __init__(self, n=10) -> None:
-        super().__init__(n)
+stack = Stack(10)
 
-    def isFull(self , item):
-        if super().isFull():
-            self.temp = [None] * 2 * len(self.nums) 
-            self.nums.extend(self.temp)
-        super().push(item)
-        
-# stack = Stack(10)
+print(stack.push(5))
+print(stack.push(10))
+print(stack.push(15))
+print(stack.push(20))
+print(stack.push(25))
+print(stack.push(30))
+print(stack.push(35))
+print(stack.push(40))
+print(stack.push(45))
+print(stack.push(50))
 
-# print(stack.push(5))
-# print(stack.push(10))
-# print(stack.push(15))
-# print(stack.push(20))
-# print(stack.push(25))
-# print(stack.push(30))
-# print(stack.push(35))
-# print(stack.push(40))
-# print(stack.push(45))
-# print(stack.push(50))
+print(stack.push(55)) # Overflow call
 
-# print(stack.push(55)) # Overflow call
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
 
-# print(stack.pop())
-# print(stack.pop())
-# print(stack.pop())
-# print(stack.pop())
-# print(stack.pop())
-# print(stack.pop())
-# print(stack.pop())
-# print(stack.pop())
-# print(stack.pop())
-# print(stack.pop())
+print(stack.peek()) # 0 elements case
 
-# print(stack.peek()) # 0 elements case
+print(stack.pop()) # Stack empty
 
-# print(stack.pop()) # Stack empty
-
-dstack = DynamicStack(5)
 
 
